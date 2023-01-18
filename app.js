6f667424f9890ec894070312515b1fb6b61189d6
Mariano Bruno <mariano.-ezequiel@hotmail.com>
2023-01-18 17:07:57 -0300

    Desafio login

--- app.js
+++ app.js
@@ -6,6 +6,23 @@ const TableManager = require('./controllers/tableManager.js')
 const optionChat = require('./options/chatDB.js')
 const optionProd = require('./options/productDb.js')
 const fakerProducts = require('./options/fakerProducts')
+const session = require('express-session')
+const FileStore = require('session-file-store')
+const cookieParser = require("cookie-parser");
+
+const Store = FileStore(session)
+
+app.use(cookieParser());
+
+app.use(session({
+    store: new Store({
+        path: './sessions',
+        ttl: 60,
+    }),
+    secret: 'b4ck3end',
+    resave: false,
+    saveUninitialized: false
+}))
 
 const server = app.listen(8080, () => console.log('Server UP'))
 const io = new Server(server)
@@ -23,10 +40,35 @@ app.engine('handlebars', handlebars.engine())
 app.set('views', 'public/views')
 app.set('view engine', 'handlebars')
 
-app.get('/', async (req, resp)=>{
+
+const loginChecker = (req, res, next)=>{
+    if(!req.session.user){
+        res.redirect('/login')
+    } else{
+        next()
+    }
+}
+
+app.get('/login', async (req, resp)=>{
+    resp.render('login')
+})
+
+app.get('/', loginChecker, async (req, resp)=>{
     await manejadorProductos.createTable()
     await manejadorChat.createTable()
-    resp.render('home')
+    resp.render('home', {usuario: req.session.user})
+})
+
+app.post('/', async (req, resp)=>{
+    req.session.user = req.body.user
+    resp.redirect('/')
+})
+
+app.get('/logout', async (req, resp)=>{
+    let usuarioBorrado = req.session.user
+    resp.clearCookie('connect.sid')
+    req.session.destroy()
+    resp.render('logout', {usuario: usuarioBorrado})
 })
 
 app.get('/api/productos-test', async (req, resp)=>{
@@ -55,5 +97,5 @@ io.on('connection', async socket =>{
         historial = await manejadorChat.getInfo()
         io.emit("history", historial)
     })
-    
+   
 })
